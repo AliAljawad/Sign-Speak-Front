376fc62cfdca00cae54e9830feb73bb4ed06473e
Ali Aljawad <alialjawad2k1@gmail.com>
2024-08-29 02:51:14 +0300

    added camera support and initialize with first available camera

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:sign_speak/widgets/bottom_navigation_bar.dart';
+import 'package:camera/camera.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras();
+  final firstCamera = cameras.first;
+  runApp(MyApp(camera: firstCamera));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final CameraDescription camera;
+
+  const MyApp({super.key, required this.camera});
 
   @override
   Widget build(BuildContext context) {
-    return  const MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyBottomNavigationBar(),  
+      home: MyBottomNavigationBar(camera: camera)  
     );
     }
 }
