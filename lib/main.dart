6c6f0236d7393c6cb0705bd0632686c2aa893c8e
Ali Aljawad <alialjawad2k1@gmail.com>
2024-08-29 00:30:29 +0300

    loaded the signup screen in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sign_speak/screens/login_screen.dart';
+import 'package:sign_speak/screens/sign_up_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: SignUpScreen(),
     );
     }
 }
