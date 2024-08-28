da526f347fcb930abf32fc6777df0b956108a432
Ali Aljawad <alialjawad2k1@gmail.com>
2024-08-29 00:39:39 +0300

    increased the font of the text in the appbar

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -8,7 +8,13 @@ class SignUpPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text('Sign up'),
+        title: const Text(
+          'Sign up',
+          style: TextStyle(
+            fontSize: 30, // Match the size of the login text
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         centerTitle: true,
         backgroundColor: Colors.white,
         elevation: 0,
@@ -18,72 +24,71 @@ class SignUpPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-  Image.asset(
-    'assets/images/logo.png',
-    height: 150,
-  ),
-  const SizedBox(height: 20),
-  const Text(
-    'Create Account',
-    style: TextStyle(
-      fontSize: 24,
-      fontWeight: FontWeight.bold,
-    ),
-  ),
-   const SizedBox(height: 20),
-  const TextField(
-    decoration: InputDecoration(
-      labelText: 'Email',
-      prefixIcon: Icon(Icons.email),
-      border: OutlineInputBorder(),
-    ),
-  ),
-   const SizedBox(height: 10),
-  const TextField(
-    obscureText: true,
-    decoration: InputDecoration(
-      labelText: 'Password',
-      prefixIcon: Icon(Icons.lock),
-      border: OutlineInputBorder(),
-    ),
-  ),
-  const SizedBox(height: 10),
-  const TextField(
-    obscureText: true,
-    decoration: InputDecoration(
-      labelText: 'Confirm Password',
-      prefixIcon: Icon(Icons.lock),
-      border: OutlineInputBorder(),
-    ),
-  ),
-   const SizedBox(height: 20),
-  ElevatedButton(
-    onPressed: () {},
-    style: ElevatedButton.styleFrom(
-      backgroundColor: Colors.blue,
-      padding: const EdgeInsets.symmetric(vertical: 15),
-      minimumSize: const Size(double.infinity, 0),
-    ),
-    child: const Text(
-      'Sign Up',
-      style: TextStyle(
-        color: Colors.white,
-        fontSize: 16,
-      ),
-    ),
-  ),
-  const SizedBox(height: 20),
-  TextButton(
-    onPressed: () {},
-    child: const Text(
-      "Already have an account? Log in",
-      style: TextStyle(
-        color: Colors.blue,
-      ),
-    ),
-  ),
-],
-
+            Image.asset(
+              'assets/images/logo.png', // Update with the correct path to your logo image
+              height: 150,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Create Account',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                prefixIcon: Icon(Icons.email),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 10),
+            const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                prefixIcon: Icon(Icons.lock),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 10),
+            const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Confirm Password',
+                prefixIcon: Icon(Icons.lock),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                minimumSize: const Size(double.infinity, 0),
+              ),
+              child: const Text(
+                'Sign Up',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                "Already have an account? Log in",
+                style: TextStyle(
+                  color: Colors.blue,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
