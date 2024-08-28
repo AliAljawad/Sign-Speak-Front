46996f0c94517f3a78aa63e0a9f7ded196b51fa4
Ali Aljawad <alialjawad2k1@gmail.com>
2024-08-29 00:34:08 +0300

    added logo and title to SignUpPage

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -17,7 +17,21 @@ class SignUpPage extends StatelessWidget {
         padding: const EdgeInsets.all(20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [],
+          children: [
+  Image.asset(
+    'assets/images/logo.png',
+    height: 150,
+  ),
+  const SizedBox(height: 20),
+  const Text(
+    'Create Account',
+    style: TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+    ),
+  ),
+],
+
         ),
       ),
     );
