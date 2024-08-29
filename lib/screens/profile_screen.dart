46e2bbde40576174e203cc500c833673a12eed32
Ali Aljawad <alialjawad2k1@gmail.com>
2024-08-29 15:43:04 +0300

    created basic ProfilePage class with AppBar and placeholder text

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -4,12 +4,19 @@ class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
-  State<ProfilePage> createState() => _ProfilePageState();
+  _ProfilePageState createState() => _ProfilePageState();
 }
 
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+      ),
+      body: const Center(
+        child: Text('Profile Page'),
+      ),
+    );
   }
 }
