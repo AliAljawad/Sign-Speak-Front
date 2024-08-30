06fbfc9689f4df644bcedc51841ba38679a0fb83
Ali Aljawad <alialjawad2k1@gmail.com>
2024-08-30 13:19:33 +0300

    scaffold the initial page structure

--- lib/screens/media_translation_screen.dart
+++ lib/screens/media_translation_screen.dart
@@ -4,12 +4,22 @@ class MediaTranslationPage extends StatefulWidget {
   const MediaTranslationPage({super.key});
 
   @override
-  State<MediaTranslationPage> createState() => _MediaTranslationPageState();
+  _MediaTranslationPageState createState() => _MediaTranslationPageState();
 }
 
 class _MediaTranslationPageState extends State<MediaTranslationPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Translate your media'),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Center(
+          child: Text('Media Preview'),
+        ),
+      ),
+    );
   }
 }
