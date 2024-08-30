b6fc71921beb5ad5e2aa5c3a45276cfd5ea85680
Ali Aljawad <alialjawad2k1@gmail.com>
2024-08-30 12:08:38 +0300

    added mock Data for Translation History

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -2,9 +2,30 @@ import 'package:flutter/material.dart';
 
 class HistoryPage extends StatelessWidget {
   const HistoryPage({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, dynamic>> translationHistory = [
+  {
+    'date': '2023-10-01',
+    'originalType': 'image',
+    'originalUrl': 'https://via.placeholder.com/150',
+    'translatedText': 'Hola, ¿cómo estás?',
+  },
+  {
+    'date': '2023-10-02',
+    'originalType': 'video',
+    'originalUrl': 'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4',
+    'translatedText': '¡Buenos días!',
+  },
+  {
+    'date': '2023-10-03',
+    'originalType': 'image',
+    'originalUrl': 'https://via.placeholder.com/150',
+    'translatedText': '¡Gracias!',
+  },
+];
     return Scaffold(
       appBar: AppBar(
         title: const Text('Translation History'),
