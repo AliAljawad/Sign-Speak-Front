323f8f8539e7997b04366dbe4aef170ced283273
Ali Aljawad <alialjawad2k1@gmail.com>
2024-08-31 12:57:42 +0300

    removed Camera Initialization from main

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:sign_speak/screens/login_screen.dart';
-import 'package:sign_speak/screens/sign_up_screen.dart';
 import 'package:sign_speak/widgets/bottom_navigation_bar.dart';
-import 'package:camera/camera.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final cameras = await availableCameras();
-  final firstCamera = cameras.first;
-  runApp(MyApp(camera: firstCamera));
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final CameraDescription camera;
-
-  const MyApp({super.key, required this.camera});
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  const MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage()
+      home: MyBottomNavigationBar(),
     );
-    }
+  }
 }
-
