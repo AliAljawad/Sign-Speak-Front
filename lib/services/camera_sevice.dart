23ad8f59076b5d86eae45510fb1c9645b1967df7
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-03 19:12:23 +0300

    remove unused CameraService class

--- lib/services/camera_sevice.dart
+++
@@ -1,8 +0,0 @@
-import 'package:camera/camera.dart';
-
-class CameraService {
-  Future<CameraDescription> initializeCamera() async {
-    final cameras = await availableCameras();
-    return cameras.first;
-  }
-}
