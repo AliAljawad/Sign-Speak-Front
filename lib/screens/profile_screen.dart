473de3da66b41a71304dd9571276f94017e74e58
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-05 00:06:27 +0300

    imported http.dart

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
