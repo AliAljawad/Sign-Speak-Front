ad6481e12a2adabbdbe660161f0146d3f38a1f8d
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-04 22:15:58 +0300

    improved email validation in login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -137,14 +137,22 @@ class _LoginPageState extends State<LoginPage>
               ),
             ),
             const SizedBox(height: 20),
-            TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                prefixIcon: Icon(Icons.email),
-                border: OutlineInputBorder(),
+            TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  prefixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  } else if (!RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
+                    return 'Please enter a valid email';
+                  }
+                  return null;
+                },
               ),
-            ),
             const SizedBox(height: 10),
             TextField(
               controller: _passwordController,
