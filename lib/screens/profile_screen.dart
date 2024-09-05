0ab20283c48ccdd4953d36332f91b865b8f89cae
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-05 17:36:11 +0300

    updated error handling in _updateProfile method

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -114,14 +114,12 @@ final TextEditingController _passwordController = TextEditingController();
     );
 
     if (response.statusCode == 200) {
-      final snackBar = SnackBar(content: Text('Profile updated successfully'));
-      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-    } else {
-      print(response.reasonPhrase);
-      final snackBar =
-          SnackBar(content: Text('Update failed: ${response.reasonPhrase}'));
-      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-    }
+  final snackBar = SnackBar(content: Text('Profile updated successfully'));
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+} else {
+  final snackBar = SnackBar(content: Text('Update failed: ${response.reasonPhrase}'));
+  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+}
 
     setState(() {
       _isLoading = false;
