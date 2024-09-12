5031a3140d0fab48c3be7d0db3b3074b88c44a96
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-12 19:05:26 +0300

    Remove error handling for snapshot in history screen

--- lib/screens/history_screen.dart
+++ lib/screens/history_screen.dart
@@ -62,8 +62,6 @@ class _HistoryPageState extends State<HistoryPage> {
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
             return const Center(child: Text('No translation history found.'));
           } else {
