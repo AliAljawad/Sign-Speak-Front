563308bb3b2b66bb0ad6f08d1eba7fb8a89673e4
Ali Aljawad <alialjawad2k1@gmail.com>
2024-09-14 00:39:44 +0300

    Implement user role and token check in bottom navigation bar

--- lib/widgets/bottom_navigation_bar.dart
+++ lib/widgets/bottom_navigation_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:sign_speak/screens/history_screen.dart';
 import 'package:sign_speak/screens/live_translation_screen.dart';
 import 'package:sign_speak/screens/media_translation_screen.dart';
@@ -14,21 +15,48 @@ class MyBottomNavigationBar extends StatefulWidget {
 class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
   int _selectedIndex = 0;
   List<Widget>? _pages;
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+  String? _userRole;
+  String? _jwtToken;
 
   @override
   void initState() {
     super.initState();
-    _initializeCamera();
+    _checkUserRoleAndToken();
   }
 
-  Future<void> _initializeCamera() async {
+  Future<void> _checkUserRoleAndToken() async {
+    // Retrieve the JWT token and user role from secure storage
+    _jwtToken = await _storage.read(key: 'jwt_token');
+    _userRole = await _storage.read(key: 'user_role');
+
+    if (_jwtToken == null || _userRole == null) {
+      // Handle missing token/role, maybe redirect to login
+      
+      print('No token or user role found');
+      // Redirect to login or show error message
+    } else {
+      _initializePages();
+    }
+  }
+
+  void _initializePages() {
     setState(() {
-      _pages = [
-        // const LiveTranslationScreen(),
-        const MediaTranslationPage(),
-        const HistoryPage(),
-        const ProfilePage(),
-      ];
+      if (_userRole == 'regular') {
+        _pages = [
+          const LiveTranslationScreen(), // Home for regular user
+          const MediaTranslationPage(), 
+          const HistoryPage(),
+          const ProfilePage(),
+        ];
+      } else if (_userRole == 'mute') {
+        _pages = [
+          // Define non-verbal user-specific pages
+          const LiveTranslationScreen(), 
+          const HistoryPage(),
+          const ProfilePage(),
+        ];
+      }
     });
   }
 
@@ -55,6 +83,10 @@ class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.videocam),
+            label: "Media",
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.history),
